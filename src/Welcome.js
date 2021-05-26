573c3adcba2d7e011805f95b431ef237050dc710
Lars Middleton <lars@middletonia.ca>
2021-05-26 08:40:36 -0400

    add colophon to Welcome view

--- src/Welcome.js
+++ src/Welcome.js
@@ -12,6 +12,19 @@ class Welcome extends Component {
                 <p>Play the game by guessing which of two Cat images is a real photo, and which is AI-generated.</p>
                 <p>Do well, and your high score may be immortalized in the top 5 on the Score Board!</p>
                 <Link to="/setup">Set up to play!</Link>
+                <Link to="/scores">See the Score Board!</Link>
+
+                <div className="colophon">
+                  <h3>Colophon</h3>
+                  <p>This project was bootstrapped with <a href="https://github.com/facebook/create-react-app" target="_blank">Create React App</a> and uses React-Router and Firebase, among other libraries.</p>
+                  <p>Services used include:</p>
+                  <ul>
+                    <li><a href="https://github.com/" rel="noreferrer" target="_blank">GitHub</a> for my git repo and authentication</li>
+                    <li><a href="https://firebase.google.com/" rel="noreferrer" target="_blank">Firebase</a> for Realtime Database</li>
+                    <li><a href="https://thispersondoesnotexist.com/" rel="noreferrer" target="_blank">This Person Does Not Exist</a> for GAN images</li>
+                    <li><a href="https://cataas.com/" rel="noreferrer" target="_blank">Cat as a service</a> for real cat photos</li>
+                  </ul>
+                </div>
             </section>
         );
     }
